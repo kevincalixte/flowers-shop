cf637fcf7d635be01e8c62a4ae4681167d61d363
juliettedef <juliette.defrance@outlook.fr>
2026-01-13 19:58:25 +0100

    création de la fonction getTasksByUser

--- views/employe.php
+++ views/employe.php
@@ -1,1 +1,12 @@
 // dashboard employés sur lequel ils pourront consulter et valider les tâches
+
+<?php
+
+session_start();
+
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../models/getTasksByUser.php';
+
+$userId = $_SESSION['user_id'];
+$tasks = getTasksByUser($pdo, $userId);
+?>
